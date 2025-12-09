d0ccbd1347c7cd1e6cd8a2dc7ca7565ea88412a4
Esan Samuel <esansamuel555@gmail.com>
2025-12-09 22:57:06 +0100

    added my deployed frontend url to cors

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,10 @@ const PORT = 5000;
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "https://jobsmart-two.vercel.app",
+      "http://localhost:3000",
+    ],
     methods: ["GET", "POST"],
   },
 });
